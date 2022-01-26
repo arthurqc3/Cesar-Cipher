7006352320fec601e3fe08b6e4dea91bd46a8cf0
Arthur Quintela <86750553+arthurqc3@users.noreply.github.com>
2022-01-26 21:27:41 +0000

    consertado o continuar e deixando o código mais clean

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from art import logo
-
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from alphabet import alphabet
 
 print(logo)
-direction = input("Digite 'codificar' para encriptar, digite 'decodificar' para desencriptar:\n")
-text = input("Digite sua mensagem:\n").lower()
-shift = int(input("Digite o numero para trocar de posição:\n"))
 
 def ceasar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decodificar":
-    shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    new_letter = alphabet[new_position]
-    end_text += new_letter
+   shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
   print(f"O texto resultante é {end_text}")
 
-ceasar(start_text = text, shift_amount = shift, cipher_direction = direction)
+again = True
+
+while again:
+  direction = input("Digite 'codificar' para encriptar, digite 'decodificar' para desencriptar:\n")
+  text = input("Digite sua mensagem:\n").lower()
+  shift = int(input("Digite o numero para trocar de posição:\n"))
+  shift = shift % 26   
+
+  ceasar(start_text = text, shift_amount = shift, cipher_direction = direction)
+
+  result = input("Você desejar usar novamente? digite SIM ou NAO\n").lower()
+  if result == "nao":
+    print("Até mais o/")
+    again = False
